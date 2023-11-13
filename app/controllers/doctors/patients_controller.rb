68523aef05b7861178b8a7849edc42b9183b73da
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-13 10:57:51 -0700

    Debug doctor_patient destroy controller action

--- app/controllers/doctors/patients_controller.rb
+++ app/controllers/doctors/patients_controller.rb
@@ -2,10 +2,12 @@ class Doctors::PatientsController < ApplicationController
   def destroy
     doctor = Doctor.find(params[:doctor_id])
     patient = Patient.find(params[:id])
-    DoctorPatient.destroy({
+    doctor_patient = DoctorPatient.find_by({
       doctor: doctor, 
       patient: patient
     })
+    
+    doctor_patient.destroy
 
     redirect_to doctor_path(doctor)
   end
