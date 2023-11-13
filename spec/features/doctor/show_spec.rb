c57168d7c99758a3c0c4be3d9697b846bcc566e2
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-13 09:50:31 -0700

    Testing written

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe "doctor show" do
     @dr2 = @hospital.doctors.create!(name: "Dr. John", specialty: "Forensic Pathology", university: "UC")
     @deadperson1 = @dr2.patients.create!(name: "John Doe", age: 78)
     @deadperson2 = @dr2.patients.create!(name: "Jane Doe", age: 90)
+    @otherpatient1 = Patient.create!(name: "JS Bach", age: 65)
+    @otherpatient2 = Patient.create!(name: "Debussy", age: 55)
+    DoctorPatient.create!(doctor: @dr1, patient: @otherpatient1)
+    DoctorPatient.create!(doctor: @dr2, patient: @otherpatient1)
+    DoctorPatient.create!(doctor: @dr1, patient: @otherpatient2)
+    DoctorPatient.create!(doctor: @dr2, patient: @otherpatient2)
     #I'm not trying to be morbid, my father-in-law is a forensic pathologist, that was one of the first doctors I thought of and his patients are all dead people...
 
 
@@ -21,12 +27,23 @@ RSpec.describe "doctor show" do
   
   it 'should show the name of the hospital and patients' do
     visit "/doctors/#{@dr2.id}"
-    save_and_open_page
     expect(page).to have_content(@hospital.name)
     expect(page).to have_content(@deadperson1.name)
     expect(page).to have_content(@deadperson2.name)
   end
 
+  it 'allows you to remove a patient' do
+#     When I visit a Doctor's show page
+    visit "/doctors/#{@dr2.id}"
+    expect(page).to have_content(@otherpatient1.name)
+    expect(page).to have_button("Remove #{@otherpatient1}")
+    click_button "Remove #{@otherpatient1}"
+    expect(current_path).to eq("/doctors/#{@dr2.id}")
+    expect(page).to_not have_content(@otherpatient1.name)
+    
+    visit "/doctors/#{@dr1.id}"
+    expect(page).to have_content(@otherpatient1.name)
 
+  end
 
 end
