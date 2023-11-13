173d106c4eb7879364a1ba3227c2540b72d10c09
Taylor Pubins <tpubz@icloud.com>
2023-11-13 10:35:25 -0700

    Add patient index with spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :patients, only: [:index]
+
   resources :doctors, only: [:show] do
-    resources :patients, controller: "doctor_patients",only: [:destroy]
+    resources :patients, controller: "doctor_patients", only: [:destroy]
   end
 end
