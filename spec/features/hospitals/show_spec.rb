11453f21946aa1f7345f5dcf50a972290fc5d598
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-13 11:02:09 -0700

    Setup barebones hospital show spec

---
+++ spec/features/hospitals/show_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "the hospital show page" do
+  before :each do
+    @hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    @hospital_2 = Hospital.create!(name: "Seaside Health & Wellness Center")
+    @doctor_1 = @hospital_1.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+    @doctor_2 = @hospital_1.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+    @patient_1 = @doctor_1.patients.create!(name: "Katie Bryce", age: 24)
+    @patient_2 = @doctor_1.patients.create!(name: "Denny Duquette", age: 39)
+    @patient_3 = Patient.create!(name: "Rebecca Pope", age: 32)
+    @doctor_2.patients << @patient_1
+  end
+
+  it "" do
+
+  end
+end
