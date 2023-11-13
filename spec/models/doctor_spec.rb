224aa3330b57589b2d2e4a4f5a1cd208c2d9e5ef
Joseph Lee <jhjlee702@gmail.com>
2023-11-13 09:26:13 -0700

    set up relationships and testing

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Doctor do
-  it {should belong_to :hospital}
+  describe "relationships" do
+    it {should belong_to :hospital}
+    it {should have_many :patients}
+  end
 end
