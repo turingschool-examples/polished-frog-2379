bbadbee8b309d372fc1740a2eecd35c79f4e9b66
Nathan Trautenberg <Natester123@mac.com>
2023-11-13 09:13:20 -0700

    created patient and doctor patient tables, made keys, created further model tests.

---
+++ db/migrate/20231113160344_create_patient.rb
@@ -0,0 +1,10 @@
+class CreatePatient < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
