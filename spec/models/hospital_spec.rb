ac4924e07a0d3a045f4ae12c6f42429fe268ec35
Sooyung Kim <kimx1279@gmail.com>
2023-11-13 11:53:12 -0700

    Remove test data from hospital model spec

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
-  before :each do
-    test_data
-  end
-
   it {should have_many :doctors}
   it {should have_many(:patients).through(:doctors)}
 end
