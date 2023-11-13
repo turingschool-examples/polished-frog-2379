9280227514553bc52f42322f445961c0c24144d9
Xander Hendry <vhendry.coding@gmail.com>
2023-11-13 13:11:55 -0500

    add test for Hospital method #doctors_with_patient_count

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Doctor do
   it {should have_many :doctor_patients}
   it {should have_many(:patients).through(:doctor_patients)}
 
+
+  describe "instance methods" do 
+    before(:each) do 
+      load_test_data
+    end
+
+    describe "#doctors_with_patient_count" do 
+      it "finds all doctors working at a hospital and the number of patients each doctor sees, ordered by patient count" do 
+        doctors_and_patients = @hospital1.doctors_with_patient_count
+        expect(doctors_and_patients.first.name).to eq(@doctor2.name)
+        expect(doctors_and_patients.first.patient_count).to eq(3)
+      end
+    end
+  end
 end
