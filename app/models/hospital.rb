d8c256a4be8a775f6c40887418c0184403f3ddb0
Nathan Trautenberg <Natester123@mac.com>
2023-11-13 11:57:21 -0700

    made model test for doctor patient counts.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -3,14 +3,13 @@ class Hospital < ApplicationRecord
   has_many :patients, through: :doctors
 
   def doctor_patient_counts
-    #require 'pry'; binding.pry
     self.doctors
     .joins(:patients)
     .group('doctors.id')
     .select('doctors.name, COUNT(patients.id) AS patient_count')
     .order('patient_count DESC')
 
-  #     SELECT doctors.name, COUNT(patients.id) AS patient_count
+  # SELECT doctors.name, COUNT(patients.id) AS patient_count
   # FROM doctors
   # INNER JOIN doctor_patients ON doctor_patients.doctor_id = doctors.id
   # INNER JOIN patients ON patients.id = doctor_patients.patient_id
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Hospital do
   describe "relationships" do
     it {should have_many :doctors}
   end
+
+  describe "instance methods" do
+    before :each do 
+      test_data
+    end
+
+    it "doctor patient counts" do
+      expected_names = ["Meredith Grey", "Alex Karev"]
+      actual = []
+      @hospital1.doctor_patient_counts.each do |d|
+        actual << d.name
+      end
+      expect(actual).to eq(expected_names)
+    end
+  end
 end
