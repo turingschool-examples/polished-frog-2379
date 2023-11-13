eb8a635216bb5cae53bf3a62838446b32afe2e12
Miranda Delaney <delaney.miranda1@gmail.com>
2023-11-13 13:28:30 -0700

    add model testing

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,4 +3,14 @@ require 'rails_helper'
 RSpec.describe Patient do
   it { should have_many(:doctor_patients) }
   it { should have_many(:doctors).through(:doctor_patients) }
+
+  describe "#adults" do
+    it "displays all patients over 18 and in alphabetical order" do
+      @patient1 = Patient.create!(name: "Katie Bryce", age: 24)
+      @patient2 = Patient.create!(name: "Denny Duquette", age: 39)
+      @patient3 = Patient.create!(name: "Rebecca Pope", age: 32)
+      @patient4 = Patient.create!(name: "Zola Shephard", age: 2)
+      expect(Patient.adults).to eq([@patient2, @patient1, @patient3])
+    end
+  end
 end
