5ea09b7eb0b5b75102788f59bcc840fa6ae53680
Xander Hendry <vhendry.coding@gmail.com>
2023-11-13 12:27:58 -0500

    add tests for patient index

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper' 
+
+RSpec.describe "Patients Index Page (/patients)" do 
+  before(:each) do 
+   load_test_data 
+  end
+  describe "visiting the Patients index page" do 
+    it "displays the names of all adult patients in alphabetical order" do 
+      visit "/patients"
+
+      expect(page).to have_content("Patients Index")
+      expect(page).to have_content(@patient1.name)
+      expect(page).to have_content(@patient2.name)
+      expect(page).to have_content(@patient3.name)
+      expect(page).to have_content(@patient4.name)
+      this = @patient2.name
+      that = @patient1.name
+      expect(this).to appear_before(that)
+    end
+    it "will not display the names of Patients under 18" do 
+      visit "/patients"
+
+      expect(page).to_not have_content(@patient5.name)
+      expect(page).to_not have_content(@patient6.name)
+      expect(page).to_not have_content(@patient7.name)
+    end
+  end
+end 
